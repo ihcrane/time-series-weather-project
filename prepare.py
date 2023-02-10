e71d09c84cfe5458d8beb10f7ad7993c59d0b920
Ian Crane <ian.crane12@gmail.com>
2023-02-10 11:33:13 -0600

    completed project

--- prepare.py
+++ prepare.py
@@ -9,6 +9,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import QuantileTransformer
 
+from math import sqrt
+from sklearn.metrics import mean_squared_error
+
 
 def train_val_test(df, target=None, stratify=None, seed=42):
     
@@ -268,4 +271,44 @@ def plot_categorical_and_continuous_vars(train, cont_vars, cat_vars):
 def plot_variable_pairs(train):
     sns.pairplot(data=train.sample(2000), diag_kind='hist', kind='reg')
     plt.show()
+    
+
+def evaluate(target_var, val, yhat_df):
+    '''
+    This function will take the actual values of the target_var from validate, 
+    and the predicted values stored in yhat_df, 
+    and compute the rmse, rounding to 0 decimal places. 
+    it will return the rmse. 
+    '''
+    rmse = round(sqrt(mean_squared_error(val[target_var], yhat_df[target_var])), 0)
+    return rmse
+
+
+def plot_and_eval(target_var, train, val, yhat_df):
+    '''
+    This function takes in the target var name (string), and returns a plot
+    of the values of train for that variable, validate, and the predicted values from yhat_df. 
+    it will als lable the rmse. 
+    '''
+    plt.figure(figsize = (12,4))
+    plt.plot(train[target_var], label='Train', linewidth=1, color='#377eb8')
+    plt.plot(val[target_var], label='Validate', linewidth=1, color='#ff7f00')
+    plt.plot(yhat_df[target_var], label='yhat', linewidth=2, color='#a65628')
+    plt.legend()
+    plt.title(target_var)
+    rmse = evaluate(target_var, val, yhat_df)
+    print(target_var, '-- RMSE: {:.0f}'.format(rmse))
+    plt.show()
+    
+    
+def append_eval_df(model_type, target_var, val, yhat_df, eval_df):
+    '''
+    this function takes in as arguments the type of model run, and the name of the target variable. 
+    It returns the eval_df with the rmse appended to it for that model and target_var. 
+    '''
+    rmse = evaluate(target_var, val, yhat_df)
+    d = {'model_type': [model_type], 'target_var': [target_var],
+        'rmse': [rmse]}
+    d = pd.DataFrame(d)
+    return eval_df.append(d, ignore_index = True)
     
